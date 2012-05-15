f246f47eda76ff7501df49c5ef7ea71f2a000974
Mattijs Ugen <mattijsugen@gmail.com>
2012-05-15 16:22:49 +0200

    Bump version to 0.3

--- spark.py
+++ spark.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=UTF-8
 
-__version__ = (0, 2, 2)
+__version__ = (0, 3)
 __version_string__ = '.'.join(map(str, __version__))
 
 # taken from https://github.com/holman/spark
